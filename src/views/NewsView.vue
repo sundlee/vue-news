6a05c8d683dcf0d41d93eb6f816b08bd51a70762
sundlee <sundlee@gmail.com>
2022-03-25 22:03:46 +0900

    뉴스 리스트 스타일링

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="news-view">
-    <p
-      v-for="(item, index) in $store.state.news"
-      :key="index"
-    >
-      <a :href="item.url">{{ item.title }}</a>
-      <small>{{ item.time_ago }} by
-        <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-      </small>
-    </p>
+  <div>
+    <ul class="news-list">
+      <li
+        class="post"
+        v-for="(item, index) in $store.state.news"
+        :key="index"
+      >
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a :href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by <router-link class="news-title" :to="`/user/${item.user}`">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -21,6 +33,29 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
