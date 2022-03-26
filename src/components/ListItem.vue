86fbbcdfe155138162e70c5d94faf6d7a73b4512
sundlee <sundlee@gmail.com>
2022-03-26 09:33:35 +0900

    공통 컴포넌트 ListItem 제작

---
+++ src/components/ListItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <ul class="news-list">
+      <li
+        class="post"
+        v-for="(item, index) in $store.state.news"
+        :key="index"
+      >
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a :href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by <router-link class="news-title" :to="`/user/${item.user}`">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  created() {
+    this.$store.dispatch('FETCH_NEWS');
+  },
+}
+</script>
+
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
