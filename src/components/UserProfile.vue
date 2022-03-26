163a4d0bc4258c76b9eccfd15ae47ed07ba87878
sundlee <sundlee@gmail.com>
2022-03-26 13:02:52 +0900

    사용자 프로필 컴포넌트 소개 및 등록

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="fa-solid fa-user"></i>
+    </div>
+    <div class="user-description">
+      hello
+      <!-- <router-link :to="`/user/${fetchedItem.user}`">
+        {{ fetchedItem.user }}
+      </router-link>
+      <div class="time">
+        {{ fetchedItem.time_ago }}
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserProfile",
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <p>name: {{ userInfo.id }}</p>
+    <user-profile></user-profile>
+    <!-- <p>name: {{ userInfo.id }}</p>
     <p>karma: {{ userInfo.karma }}</p>
-    <p>created: {{ userInfo.created }}</p>
+    <p>created: {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
+import UserProfile from '@/components/UserProfile.vue'
 
 export default {
   name: 'UserView',
+    components: {
+    UserProfile,
+  },
   computed: {
     userInfo() {
       return this.$store.state.user;
